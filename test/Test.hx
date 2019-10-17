92e2e2915284481975b8cf15017296d777eb33b5
Damilare Akinlaja <darmie@akinlaja.com>
2019-10-17 10:46:08 +0100

    minor fix

--- test/Test.hx
+++ test/Test.hx
@@ -8,29 +8,29 @@ import sys.io.File;
 
 class Test {
 	public static function main() {
-        var source = FileSystem.fullPath("test/program.wasm");
-        var raw = File.getBytes(source);
+		var source = FileSystem.fullPath("test/program.wasm");
+		var raw = File.getBytes(source);
 		var vm = new VM(raw, {
-            disableFloatingPoint: false,
-            maxMemoryPages: 1024,
-            maxCallStackDepth: 0,
-            maxValueSlots: 0
-        }, new NopResolver());
+			disableFloatingPoint: false,
+			maxMemoryPages: 1024,
+			maxCallStackDepth: 0,
+			maxValueSlots: 0
+		}, new NopResolver());
 
 		var funcID = vm.getFunctionExport("main");
 		var data = vm.run(funcID, []);
 		if (data.err != null) {
-            trace(data.err);
+			trace(data.err);
 			// do something with error
 		}
-        var val:I64 = data.result;
-        
-        Sys.println('=== Executing Program.wasm ===\n');
-        var buf = new StringBuf();
-        Writer.writeTo(buf, vm.module.base);
-        Sys.println(buf.toString());
-        Sys.println('==============================');
+		var val:I64 = data.result;
+
+		Sys.println('=== Executing Program.wasm ===\n');
+		var buf = new StringBuf();
+		Writer.writeTo(buf, vm.module.base);
+		Sys.println(buf.toString());
+		Sys.println('==============================');
 		Sys.println('Output = $val');
-        Sys.println('==============================');
+		Sys.println('==============================');
 	}
 }
